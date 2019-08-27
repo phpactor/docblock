8ce19f1c95794d6b51409fb630e4a9b45ddfb6f2
USAMI Kenta <tadsan@zonu.me>
2019-08-27 19:52:38 +0900

    Extract mask chars to const for trim() instead of string literal

--- lib/Parser/TypesParser.php
+++ lib/Parser/TypesParser.php
@@ -7,6 +7,14 @@ use Phpactor\Docblock\DocblockTypes;
 
 class TypesParser
 {
+    /**
+     * Mask to remove white space and nullable prefixes(?)
+     *
+     * The current Phpactor's type system doesn't handle "nullable",
+     * it just erases at the same time as white space.
+     */
+    private const MASK_WHITESPACE_AND_IGNORED_PREFIX = "? \t\n\r\0\x0B";
+
     public function parseTypes(string $types): DocblockTypes
     {
         if (empty($types)) {
@@ -17,17 +25,17 @@ class TypesParser
         $docblockTypes = [];
 
         foreach (explode('|', $types) as $type) {
-            $type = trim($type, "? \t\n\r\0\x0B");
+            $type = trim($type, self::MASK_WHITESPACE_AND_IGNORED_PREFIX);
 
             if (preg_match('{^(.*)<(.*)>$}', $type, $matches)) {
                 $type = $matches[1];
-                $collectionType = trim($matches[2], "? \t\n\r\0\x0B");
+                $collectionType = trim($matches[2], self::MASK_WHITESPACE_AND_IGNORED_PREFIX);
                 $docblockTypes[] = DocblockType::collectionOf($type, $collectionType);
                 continue;
             }
 
             if (substr($type, -2) == '[]') {
-                $type = trim(substr($type, 0, -2), "? \t\n\r\0\x0B");;
+                $type = trim(substr($type, 0, -2), self::MASK_WHITESPACE_AND_IGNORED_PREFIX);
                 $docblockTypes[] = DocblockType::arrayOf($type);
                 continue;
             }
